49861ca22095a2863b7c33628487118caeb1cfb6
asha <jtc.21.am@gmail.com>
2022-02-07 13:40:11 -0500

    Week 2 Challenge 1 Part A

---
+++ Week2_Challenge1-A.py
@@ -0,0 +1,38 @@
+#Asha Maurya - Week 2 - Challenge 1
+
+#print('1: Describe what is happening below by adding comments before each line')
+#Assign the first variable
+box_1 = 'books'
+#Assign the second variable
+box_2 = 'clothes'
+#Assign the third variable
+box_3 = 'plants'
+#Assign the fourth variable
+box_4 = 'kitchen stuff'
+
+#Print the four variables as a list with comma separators
+#print('2: Print the variables box_1, box_2, box_3, box_4')
+print(box_1+", "+box_2+", "+box_3+", "+box_4)
+
+#Assign a new variable (fifth)
+#print('3.1: Declare a variable with the name "address", assign it any street name you like')
+address = 'Main Street'
+
+#Print fifth variable
+#print('3.2: Print the address variable')
+print(address)
+
+#Modify second and fourth variable, then print variable 1-4 as a list)
+##print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again')
+box_2 = 'Suit & Jacket'
+box_4 = 'Pots & Pans'
+print(box_1+", "+box_2+", "+box_3+", "+box_4)
+
+#Correct the line of code which is assigning the sixth variable
+##print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code')
+#completion message = 'Completed the first Python challenge!
+completion_message = 'Completed the first Python challenge!'
+
+#Print sixth variable
+#print('5.2: Turn the comment below back into regular Python code')
+print(completion_message)
